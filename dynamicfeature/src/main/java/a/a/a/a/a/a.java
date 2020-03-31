a819e203a8eced7d52f5316aa4239e6a71f31f67
YOUNG HO CHA <ganachoco@linecorp.com>
2020-03-31 18:50:47 +0900

    Add case for invalid class merge on R8 release mode

---
+++ dynamicfeature/src/main/java/a/a/a/a/a/a.java
@@ -0,0 +1,8 @@
+package a.a.a.a.a;
+
+public class a {
+    public static int a() {
+        System.out.println("hello world");
+        return 0;
+    }
+}
---
+++ dynamicfeature/src/main/java/com/example/dynamicfeature/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dynamicfeature;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        a.a.a.a.a.a.a();
+        super.onCreate(bundle);
+    }
+}
